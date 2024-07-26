4e1d0b21c25283b2115f3f76a022f234ef264017
Shaun Mbolompo <Shaun.Mbolompo@derivco.co.za>
2024-07-26 09:47:45 +0200

    Edit ICodeContentFactory.cs

--- CodeAPI/Models/ICodeContentFactory.cs
+++ CodeAPI/Models/ICodeContentFactory.cs
@@ -3,6 +3,7 @@ namespace CodeAPI.Models;
 public interface ICodeContentFactory
 {
     CodeContent GetCodeContentByStep(int step);
+    IEnumerable<CodeContent> GetAllCodeContents();
     void AddCodeContent(CodeContent codeContent);
     void UpdateCodeContent(CodeContent codeContent);
     void DeleteCodeContent(int id);
